33a8a17a246682498d9f18743175e451b72b0bf6
Elemental-Draco <79873861+Elemental-Draco@users.noreply.github.com>
2025-05-02 20:29:06 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HeaderTemplate from './components/HeaderTemplate.vue'
+</script>
+
+<template>
+  <HeaderTemplate />
+  <RouterView></RouterView>
+</template>
+
+<style scoped></style>
---
+++ src/components/HeaderTemplate.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <img src="../assets/knife-logo.png" alt="Logo for Quick Thaw App." />
+    <h1>Quick Thaw</h1>
+    <button><img src="../assets/setting.png" alt="Settings Menu" /></button>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: darkgray;
+}
+header img {
+  max-width: 50px;
+  max-height: 50px;
+}
+
+h1 {
+  font-size: 2rem;
+  font-weight: bold;
+}
+</style>
